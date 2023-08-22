15a312dbc6ac6ffbc9e9d7689a3c5bca2e2e7304
Joseph Okpako <komeokpako@gmail.com>
2023-08-22 10:54:48 +0000

    new file(num_parser) added to print specifier d and i

---
+++ num_parser.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * num_print - prints a number
+ * @ap: argument pointer
+ * Return: lenght of the printed arguments
+ */
+int num_print(va_list ap)
+{
+	unsigned int check_num;
+	int ap_num, divisor = 1, int_count = 0;
+
+	ap_num = va_arg(ap, int);
+
+	if (ap_num < 0)
+	{
+		int_count += _putchar('-');
+		check_num = ap_num * (-1);
+	}
+	else
+		check_num = ap_num;
+
+	while (check_num / divisor > 9)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		int_count = _putchar('0' + (check_num / divisor));
+		check_num %= divisor;
+		divisor /= 10;
+	}
+
+	return (int_count);
+}
