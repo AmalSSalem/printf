45176bcb5c43291a01c9065209d046a349a6f5bc
amalsherifsalem <amalsherifsalem@gmail.com>
2023-08-24 01:56:11 +0300

    putchar with buffer

--- _putchar.c
+++ _putchar.c
@@ -5,8 +5,21 @@
  * @c: char to print
  * Return: 1 on success, -1 on error
  */
-int _putchar(char  c)
+int _putchar(char c)
 {
-	return (write(1, c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
 
